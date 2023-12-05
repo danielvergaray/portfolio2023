291c181dce1a0ed718afe44f113b84bd8f5700b2
Daniel Vergaray <danielvergarayescobar@gmail.com>
2023-12-05 16:22:25 -0500

    Falta modificacion de popUp en desktop

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,6 +1,10 @@
 let menuHamburguesaContainer = document.getElementById("menuHamburguesaId");
-let todoContenido= document.getElementById("todoElContenido");
-let menuHambuguesaDesplegable= document.getElementById("menuDesplegable");
+let todoContenido = document.getElementById("todoElContenido");
+let headerIdHtml= document.getElementById('headerId');
+let menuHambuguesaDesplegable = document.getElementById("menuDesplegable");
+let carruselPortafolioHtml= document.getElementById('carouselExampleIndicators2');
+
+
 /* crearMenu(menuHamburguesaContainer,menuHambuguesaDesplegable); */
 
 /* function crearMenu(menu,menuDesplegable) {
@@ -32,7 +36,7 @@ menuDesplegable.classList.add("oculto");
 } */
 
 
-let btnOpcionMenu=document.querySelectorAll(".opcion-seleccionada");
+let btnOpcionMenu = document.querySelectorAll(".opcion-seleccionada");
 btnOpcionMenu.forEach((btnOpcion) => {
   btnOpcion.addEventListener("click", () =>
     mostrarOcultar(menuHamburguesaContainer, todoContenido)
@@ -42,14 +46,14 @@ btnOpcionMenu.forEach((btnOpcion) => {
 let btnHamburguesa = document.getElementById("menuHamburguesaBtn");
 /* btnHamburguesa.addEventListener("click", ()=> mostrarOcultar(menuHamburguesaContainer, todoContenido)); */
 
-function mostrarOcultar(menu, todoContenido){
-   
-    menu.classList.toggle("oculto");
-    menuDesplegable.classList.toggle("oculto");
+/* function mostrarOcultar(menu, todoContenido) {
 
-    todoContenido.classList.toggle("oculto");
+  menu.classList.toggle("oculto");
+  menuDesplegable.classList.toggle("oculto");
 
-}
+  todoContenido.classList.toggle("oculto");
+
+} */
 
 
 
@@ -156,7 +160,7 @@ const idiomas = [
     description: 'Comprensión lectora, capacidad de comunicacón básica',
     iconoVerMasHtml: 'iconoVerMasFr'
   },
-  
+
 ];
 
 
@@ -166,8 +170,8 @@ cursos.forEach(curso => {
   const cardInfo = document.getElementById(curso.cardInfo);
   const cardBody = document.getElementById(curso.cardBody);
   const cardFlip = document.getElementById(curso.cardFlip);
-  const iconoVerMasHtml= document.getElementById(curso.iconoVerMasHtml)
-  
+  const iconoVerMasHtml = document.getElementById(curso.iconoVerMasHtml)
+
 
   btn.addEventListener('click', () => flipCard(cardIconos, cardInfo, cardBody, cardFlip, curso.title, curso.institute, curso.description, iconoVerMasHtml));
 });
@@ -178,10 +182,10 @@ idiomas.forEach(idioma => {
   const cardInfo = document.getElementById(idioma.cardInfo);
   const cardBody = document.getElementById(idioma.cardBody);
   const cardFlip = document.getElementById(idioma.cardFlip);
-  const iconoVerMasHtml= document.getElementById(idioma.iconoVerMasHtml)
-  
+  const iconoVerMasHtml = document.getElementById(idioma.iconoVerMasHtml)
 
-  btn.addEventListener('click', () => flipCard(cardIconos, cardInfo, cardBody, cardFlip, idioma.title, idioma.institute, idioma.description,iconoVerMasHtml));
+
+  btn.addEventListener('click', () => flipCard(cardIconos, cardInfo, cardBody, cardFlip, idioma.title, idioma.institute, idioma.description, iconoVerMasHtml));
 });
 
 function flipCard(cardIconos, cardInfo, cardBody, cardFlip, title, institute, description, iconoFlechaVerMas) {
@@ -200,3 +204,97 @@ function flipCard(cardIconos, cardInfo, cardBody, cardFlip, title, institute, de
 }
 
 
+
+
+const proyectos = [
+  {
+    titulo: 'Adivina el Número',
+    descripcion: 'Este proyecto consiste en un juego donde se genera un numero al azar y el usuario debe adivinarlo, contando con pistas que se muestran de acuerdo al valor introducido por el usuario. Fue realizado usando javaScript.',
+    imagen: '../imagenes/desktop/adivinaElNumero.jpeg',
+    linkWeb: 'https://danielvergaray.github.io/AdivinaElNumero_juego/',
+    linkRepositorio: 'https://github.com/danielvergaray/AdivinaElNumero_juego'
+  },
+
+]
+
+
+let containerInfoProyectos = document.getElementById('infoCadaProyecto');
+
+let btnVerMasAdivinaHtml=document.getElementById('btnVerMasAdivina');
+btnVerMasAdivinaHtml.addEventListener('click', ()=> mostrarOcultarInfoProyecto(containerInfoProyectos, headerIdHtml, carruselPortafolioHtml, todoContenido))
+
+crearCardsInfoProyectos(proyectos,containerInfoProyectos)
+
+function crearCardsInfoProyectos(proyectos,containerInfoProyectos) {
+console.log("crear card")
+
+  proyectos.forEach(cadaProyecto => {
+    /* let divCreado= document.createElement('div'); */
+    /* divCreado.classList.add('oculto') */
+    containerInfoProyectos.innerHTML=` 
+    
+      <i class="fa-regular fa-circle-xmark"></i>
+      <h1>${cadaProyecto.titulo}</h1>
+      <div class="card-separador-carusel-info">
+      <div id="carouselExampleIndicators" class="carousel slide">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="..." class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="..." class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="..." class="d-block w-100" alt="...">
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+
+    <div class="cardInfo-contenedor-detalles">
+    <p>${cadaProyecto.descripcion} </p>
+      <div class="card-info-contenedorBotones">
+        <a href="${cadaProyecto.linkWeb} ">Visitar Web</a>
+        <a href="${cadaProyecto.linkRepositorio}">Ver Repositorio</a>
+      </div>
+  </div>
+
+      </div>
+      
+
+      
+   
+    
+    `
+
+    
+/* 
+    containerInfoProyectos.appendChild(divCreado) */
+  })
+
+  
+}
+
+
+function mostrarOcultarInfoProyecto(containerInfoProyectos, headerIdHtml, carruselPortafolioHtml,todoContenido) {
+console.log("mostrar")
+  containerInfoProyectos.classList.toggle("oculto");
+  headerIdHtml.classList.toggle('cortina')
+  carruselPortafolioHtml.classList.toggle('cortina')
+  todoContenido.classList.toggle('cortina')
+  /* infoCadaProyectoHtml.classList.remove('oculto') */
+  
+
+}
