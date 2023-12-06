d7b49f3650884e0a4cec7957b32b3e4db3f38fa7
Daniel Vergaray <danielvergarayescobar@gmail.com>
2023-12-06 15:57:53 -0500

    trabajando en el js para que aparezca el popUp de cada proyecto, hay un error, no quiere aparecer el popUp de los ultimos 2 proyectos

--- javaScript/script.js
+++ javaScript/script.js
@@ -3,7 +3,9 @@ let todoContenido = document.getElementById("todoElContenido");
 let headerIdHtml= document.getElementById('headerId');
 let menuHambuguesaDesplegable = document.getElementById("menuDesplegable");
 let carruselPortafolioHtml= document.getElementById('carouselExampleIndicators2');
-
+let sobreMiIdHtml= document.getElementById('sobreMiId');
+let estudiosIdHtml= document.getElementById('estudiosId');
+let contactoIdHtml= document.getElementById('contactoId');
 
 /* crearMenu(menuHamburguesaContainer,menuHambuguesaDesplegable); */
 
@@ -212,8 +214,61 @@ const proyectos = [
     descripcion: 'Este proyecto consiste en un juego donde se genera un numero al azar y el usuario debe adivinarlo, contando con pistas que se muestran de acuerdo al valor introducido por el usuario. Fue realizado usando javaScript.',
     imagen: '../imagenes/desktop/adivinaElNumero.jpeg',
     linkWeb: 'https://danielvergaray.github.io/AdivinaElNumero_juego/',
-    linkRepositorio: 'https://github.com/danielvergaray/AdivinaElNumero_juego'
+    linkRepositorio: 'https://github.com/danielvergaray/AdivinaElNumero_juego',
+    linkImg1: '../imagenes/desktop/adivinaElNumero.jpeg',
+    linkImg2: '../imagenes/desktop/adivinaElNumero.jpeg',
+    linkImg3: '../imagenes/desktop/adivinaElNumero.jpeg',
+  },{
+    titulo: '2',
+    descripcion: 'Este proyecto consiste en un juego donde se genera un numero al azar y el usuario debe adivinarlo, contando con pistas que se muestran de acuerdo al valor introducido por el usuario. Fue realizado usando javaScript.',
+    imagen: '../imagenes/desktop/adivinaElNumero.jpeg',
+    linkWeb: 'https://danielvergaray.github.io/AdivinaElNumero_juego/',
+    linkRepositorio: 'https://github.com/danielvergaray/AdivinaElNumero_juego',
+    linkImg1: '../imagenes/desktop/adivinaElNumero.jpeg',
+    linkImg2: '../imagenes/desktop/adivinaElNumero.jpeg',
+    linkImg3: '../imagenes/desktop/adivinaElNumero.jpeg',
   },
+  {
+    titulo: '3',
+    descripcion: 'Este proyecto consiste en un juego donde se genera un numero al azar y el usuario debe adivinarlo, contando con pistas que se muestran de acuerdo al valor introducido por el usuario. Fue realizado usando javaScript.',
+    imagen: '../imagenes/desktop/adivinaElNumero.jpeg',
+    linkWeb: 'https://danielvergaray.github.io/AdivinaElNumero_juego/',
+    linkRepositorio: 'https://github.com/danielvergaray/AdivinaElNumero_juego',
+    linkImg1: '../imagenes/desktop/adivinaElNumero.jpeg',
+    linkImg2: '../imagenes/desktop/adivinaElNumero.jpeg',
+    linkImg3: '../imagenes/desktop/adivinaElNumero.jpeg',
+  },
+{
+    titulo: '4',
+    descripcion: 'Este proyecto consiste en un juego donde se genera un numero al azar y el usuario debe adivinarlo, contando con pistas que se muestran de acuerdo al valor introducido por el usuario. Fue realizado usando javaScript.',
+    imagen: '../imagenes/desktop/adivinaElNumero.jpeg',
+    linkWeb: 'https://danielvergaray.github.io/AdivinaElNumero_juego/',
+    linkRepositorio: 'https://github.com/danielvergaray/AdivinaElNumero_juego',
+    linkImg1: '../imagenes/desktop/adivinaElNumero.jpeg',
+    linkImg2: '../imagenes/desktop/adivinaElNumero.jpeg',
+    linkImg3: '../imagenes/desktop/adivinaElNumero.jpeg',
+  },
+  {
+    titulo: '5',
+    descripcion: 'Este proyecto consiste en un juego donde se genera un numero al azar y el usuario debe adivinarlo, contando con pistas que se muestran de acuerdo al valor introducido por el usuario. Fue realizado usando javaScript.',
+    imagen: '../imagenes/desktop/adivinaElNumero.jpeg',
+    linkWeb: 'https://danielvergaray.github.io/AdivinaElNumero_juego/',
+    linkRepositorio: 'https://github.com/danielvergaray/AdivinaElNumero_juego',
+    linkImg1: '../imagenes/desktop/adivinaElNumero.jpeg',
+    linkImg2: '../imagenes/desktop/adivinaElNumero.jpeg',
+    linkImg3: '../imagenes/desktop/adivinaElNumero.jpeg',
+  },
+  {
+    titulo: '6',
+    descripcion: 'Este proyecto consiste en un juego donde se genera un numero al azar y el usuario debe adivinarlo, contando con pistas que se muestran de acuerdo al valor introducido por el usuario. Fue realizado usando javaScript.',
+    imagen: '../imagenes/desktop/adivinaElNumero.jpeg',
+    linkWeb: 'https://danielvergaray.github.io/AdivinaElNumero_juego/',
+    linkRepositorio: 'https://github.com/danielvergaray/AdivinaElNumero_juego',
+    linkImg1: '../imagenes/desktop/adivinaElNumero.jpeg',
+    linkImg2: '../imagenes/desktop/adivinaElNumero.jpeg',
+    linkImg3: '../imagenes/desktop/adivinaElNumero.jpeg',
+  },
+  
 
 ]
 
@@ -221,37 +276,115 @@ const proyectos = [
 let containerInfoProyectos = document.getElementById('infoCadaProyecto');
 
 let btnVerMasAdivinaHtml=document.getElementById('btnVerMasAdivina');
-btnVerMasAdivinaHtml.addEventListener('click', ()=> mostrarOcultarInfoProyecto(containerInfoProyectos, headerIdHtml, carruselPortafolioHtml, todoContenido))
+let btnVerMasMrSnackJsHtml=document.getElementById('btnVerMasMrSnackJs');
+let btnVerMasPigHtml=document.getElementById('btnVerMasPig');
+let btnVerMasMrSnackHtml=document.getElementById('btnVerMasMrSnack');
+let btnVerMasReactHtml=document.getElementById('btnVerMasReact');
+let btnVerMasWpHtml=document.getElementById('btnVerMasWp');
 
-crearCardsInfoProyectos(proyectos,containerInfoProyectos)
 
-function crearCardsInfoProyectos(proyectos,containerInfoProyectos) {
+btnVerMasAdivinaHtml.addEventListener('click', ()=>crearCardsInfoProyectos(proyectos,containerInfoProyectos, 0))
+btnVerMasMrSnackJsHtml.addEventListener('click', ()=>crearCardsInfoProyectos(proyectos,containerInfoProyectos, 1))
+btnVerMasPigHtml.addEventListener('click', ()=>crearCardsInfoProyectos(proyectos,containerInfoProyectos, 2))
+btnVerMasMrSnackHtml.addEventListener('click', ()=>crearCardsInfoProyectos(proyectos,containerInfoProyectos, 3))
+btnVerMasReactHtml.addEventListener('click', ()=>crearCardsInfoProyectos(proyectos,containerInfoProyectos, 4))
+btnVerMasWpHtml.addEventListener('click', ()=>crearCardsInfoProyectos(proyectos,containerInfoProyectos, 5))
+
+
+
+btnVerMasAdivinaHtml.addEventListener('click', ()=> mostrarOcultarInfoProyecto(containerInfoProyectos, headerIdHtml, carruselPortafolioHtml, sobreMiIdHtml, estudiosIdHtml, contactoIdHtml))
+btnVerMasMrSnackJsHtml.addEventListener('click', ()=> mostrarOcultarInfoProyecto(containerInfoProyectos, headerIdHtml, carruselPortafolioHtml, sobreMiIdHtml, estudiosIdHtml, contactoIdHtml))
+btnVerMasPigHtml.addEventListener('click', ()=> mostrarOcultarInfoProyecto(containerInfoProyectos, headerIdHtml, carruselPortafolioHtml, sobreMiIdHtml, estudiosIdHtml, contactoIdHtml))
+btnVerMasMrSnackHtml.addEventListener('click', ()=> mostrarOcultarInfoProyecto(containerInfoProyectos, headerIdHtml, carruselPortafolioHtml, sobreMiIdHtml, estudiosIdHtml, contactoIdHtml))
+btnVerMasReactHtml.addEventListener('click', ()=> mostrarOcultarInfoProyecto(containerInfoProyectos, headerIdHtml, carruselPortafolioHtml, sobreMiIdHtml, estudiosIdHtml, contactoIdHtml))
+btnVerMasWpHtml.addEventListener('click', ()=> mostrarOcultarInfoProyecto(containerInfoProyectos, headerIdHtml, carruselPortafolioHtml, sobreMiIdHtml, estudiosIdHtml, contactoIdHtml))
+
+
+
+
+/* crearCardsInfoProyectos(proyectos,containerInfoProyectos) */
+
+function crearCardsInfoProyectos(proyectos,containerInfoProyectos, index) {
 console.log("crear card")
 
-  proyectos.forEach(cadaProyecto => {
-    /* let divCreado= document.createElement('div'); */
-    /* divCreado.classList.add('oculto') */
-    containerInfoProyectos.innerHTML=` 
+let proyectos2= proyectos[index];
+console.log(proyectos2)
+
+ /*  proyectos2.forEach(cadaProyecto => {
     
-      <i class="fa-regular fa-circle-xmark"></i>
-      <h1>${cadaProyecto.titulo}</h1>
+    containerInfoProyectos.innerHTML=` 
+
+      <div id="cardIconoClose" class="card-icono-close">
+        <i class="fa-regular fa-circle-xmark"></i>
+      </div>
+      
       <div class="card-separador-carusel-info">
-      <div id="carouselExampleIndicators" class="carousel slide">
+        <div id="carouselExampleIndicators" class="carousel slide">
+          <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          </div>
+          <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="${cadaProyecto.linkImg1} " class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="${cadaProyecto.linkImg2}" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="${cadaProyecto.linkImg3}" class="d-block w-100" alt="...">
+              </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+
+        <div class="cardInfo-contenedor-detalles">
+          <h1>${cadaProyecto.titulo}</h1>
+          <p>${cadaProyecto.descripcion} </p>
+            <div class="card-info-contenedorBotones">
+              <button><a href="${cadaProyecto.linkWeb} ">Visitar Web</a></button>
+              <button><a href="${cadaProyecto.linkRepositorio}">Ver Repositorio</a></button>
+            </div>
+        </div>
+
+      </div>
+      
+    `
+
+  }) */
+
+
+  containerInfoProyectos.innerHTML=` 
+
+  <div id="cardIconoClose" class="card-icono-close">
+    <i class="fa-regular fa-circle-xmark"></i>
+  </div>
+  
+  <div class="card-separador-carusel-info">
+    <div id="carouselExampleIndicators" class="carousel slide">
       <div class="carousel-indicators">
         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
       </div>
       <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="..." class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-          <img src="..." class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-          <img src="..." class="d-block w-100" alt="...">
-        </div>
+          <div class="carousel-item active">
+            <img src="${proyectos2.linkImg1} " class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="${proyectos2.linkImg2}" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="${proyectos2.linkImg3}" class="d-block w-100" alt="...">
+          </div>
       </div>
       <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -264,37 +397,36 @@ console.log("crear card")
     </div>
 
     <div class="cardInfo-contenedor-detalles">
-    <p>${cadaProyecto.descripcion} </p>
-      <div class="card-info-contenedorBotones">
-        <a href="${cadaProyecto.linkWeb} ">Visitar Web</a>
-        <a href="${cadaProyecto.linkRepositorio}">Ver Repositorio</a>
-      </div>
+      <h1>${proyectos2.titulo}</h1>
+      <p>${proyectos2.descripcion} </p>
+        <div class="card-info-contenedorBotones">
+          <button><a href="${proyectos2.linkWeb} ">Visitar Web</a></button>
+          <button><a href="${proyectos2.linkRepositorio}">Ver Repositorio</a></button>
+        </div>
+    </div>
+
   </div>
-
-      </div>
-      
-
-      
-   
-    
-    `
-
-    
-/* 
-    containerInfoProyectos.appendChild(divCreado) */
-  })
-
   
+`
+
+
+  let iconoCloseHtml= document.getElementById('cardIconoClose');
+iconoCloseHtml.addEventListener('click', ()=> mostrarOcultarInfoProyecto(containerInfoProyectos, headerIdHtml, carruselPortafolioHtml,sobreMiIdHtml, estudiosIdHtml,contactoIdHtml ));
 }
 
 
-function mostrarOcultarInfoProyecto(containerInfoProyectos, headerIdHtml, carruselPortafolioHtml,todoContenido) {
-console.log("mostrar")
+
+function mostrarOcultarInfoProyecto(containerInfoProyectos, headerIdHtml, carruselPortafolioHtml,sobreMiIdHtml, estudiosIdHtml,contactoIdHtml) {
+
   containerInfoProyectos.classList.toggle("oculto");
-  headerIdHtml.classList.toggle('cortina')
-  carruselPortafolioHtml.classList.toggle('cortina')
-  todoContenido.classList.toggle('cortina')
+  headerIdHtml.classList.toggle('cortina');
+  sobreMiIdHtml.classList.toggle('cortina');
+  estudiosIdHtml.classList.toggle('cortina');
+  contactoIdHtml.classList.toggle('cortina');
+  carruselPortafolioHtml.classList.toggle('oculto')
+  /* todoContenido.classList.toggle('cortina') */
   /* infoCadaProyectoHtml.classList.remove('oculto') */
+  
   
 
 }
